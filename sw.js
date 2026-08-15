733d6fe96a7c47d83de61ddc4427ff50130269e2
omen131omen-sys <omen131omen@gmail.com>
2026-08-16 00:01:04 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Офлайн-кэш: после первого открытия приложение работает без интернета.
-const CACHE = 'colorist-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
+const CACHE = 'colorist-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon180.png', './icon512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
